9bd804f054ee16f5b49da5375028ec78f21cefab
NaP <annposillipo@gmail.com>
2023-07-04 17:56:07 +0200

    bonus

---
+++ js/bonus.js
@@ -0,0 +1,47 @@
+for(let i=0; i <= 100; i++){
+    if(i%3== 0 && i%5==0 ){
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square color-fizzbuzz ">FizzBuzz</p>
+        </div>
+    `;}
+    else if(i%5 == 0){
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square color-buzz ">Buzz</p>
+        </div>
+    `;}
+    else if(i%3 ==0){
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square color-fizz ">Fizz</p>
+        </div>
+    `;}
+    else{
+        document.getElementById('squaresContainer').innerHTML += `
+        <div class="square">
+            <p class="text-center text-white fs-3 fw-3 vertical-align-square color">${i}</p>
+        </div>
+    `;
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+
+
+
+}
